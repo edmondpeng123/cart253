3f5c38b6db5592eb743bc9f65993fd543800e7dd
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-09-10 20:24:34 -0400

    I did the instructions challenge
    house

---
+++ topics/instructions/instructions-challenge/js/script.js
@@ -0,0 +1,109 @@
+/**
+ * Cheese
+ * edmond and abi
+ * 
+ * this abis house and a bird
+ *
+ */
+
+
+/**
+ * this is a beautiful house with a beautiful sky
+*/
+function setup() {
+    createCanvas(1000, 800);
+    background("#0055ffff");
+    console.log("hello,world");
+}
+
+
+
+function draw() {
+  
+    background("#75daffff");
+    push();
+    noStroke()
+
+
+fill("#895072ff");
+    triangle(200, 400, 300, 420, 300, 150);
+
+    fill("#ff7300ff");
+    ellipse(120, 120, 80, 80);
+
+
+
+     fill("#b6ada1ff");
+    ellipse(800, 150, 60, 60);
+
+     fill("#cac1b4ff");
+    ellipse(800, 110, 60, 60);
+
+
+
+
+
+
+
+    fill("#000000ff");
+    ellipse(790, 107, 10, 15);
+
+    fill("#000000ff");
+    ellipse(810, 107, 10, 15);
+
+      fill("#ffffffff");
+    ellipse(792, 109, 5, 5);
+
+    fill("#ffffffff");
+    ellipse(812, 110, 5, 5);
+
+     fill("#ffd519ff");
+    ellipse(800, 120, 30, 20);
+
+    fill("#ff008cff");
+    rect(400, 400, 500, 300);
+
+    fill("#620039ff");
+    rect(300, 150, 600, 250);
+
+
+     fill("#ff66edff");
+    rect(200, 400, 200, 300);
+
+    pop();
+
+    stroke(255);
+    strokeWeight(0);
+    noFill();
+    rect(400, 100, 50, 30);
+
+
+
+ fill("#231479ff");
+    rect(80, 600, 40, 60);
+
+     fill("#000000ff");
+    rect(80, 660, 10, 40);
+
+     fill("#000000ff");
+    rect(110, 660, 10, 40);
+    
+
+
+    fill("#b68552ff");
+    ellipse(100, 600, 50, 50);
+
+ fill("#000000ff");
+    ellipse(110, 600, 20, 20);
+
+    fill("#000000ff");
+    ellipse(90, 600, 20, 20);
+
+   
+
+    fill("#00af37ff");
+    rect(0, 700, 1000, 300);
+
+}
+
+
