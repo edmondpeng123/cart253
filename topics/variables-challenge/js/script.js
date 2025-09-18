8fa6737eaf11443308f45ee1c7216df9eea0abb7
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-09-17 20:12:52 -0400

    made the sky turned night and made mr.furious red

--- topics/variables-challenge/js/script.js
+++ topics/variables-challenge/js/script.js
@@ -8,20 +8,23 @@
 
 "use strict";
 
+
+let skyShade = {
+
+  fill: {
+    r: 4,
+    g: 255,
+    b: 255,
+  }
+}
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
-}
-
-
-/**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
-function draw() {
 
-}
+
+
 
 /**
  * Mr. Furious
@@ -33,16 +36,17 @@ function draw() {
 "use strict";
 
 // Our friend Mr. Furious
+
 let mrFurious = {
   // Position and size
-  x: 200,
-  y: 200,
-  size: 100,
+  x: 600,
+  y: 600,
+  size: 200,
   // Colour
   fill: {
-    r: 255,
-    g: 225,
-    b: 225
+    r: 152,
+    g: 115,
+    b: 90
   }
 };
 
@@ -50,19 +54,29 @@ let mrFurious = {
  * Create the canvas
  */
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 /**
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+
+  skyShade.fill.b = skyShade.fill.b-1
+   skyShade.fill.g = skyShade.fill.g-1
+  background(skyShade.fill.r,skyShade.fill.g,skyShade.fill.b);
+
+  mrFurious.fill.r = mrFurious.fill.r+1
+  mrFurious.fill.g = mrFurious.fill.g-1
+  mrFurious.fill.b = mrFurious.fill.b-1
   
+
+
   // Draw Mr. Furious as a coloured circle
   push();
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
-  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  ellipse(mrFurious.x/2, mrFurious.y/2, mrFurious.size);
   pop();
 }
+
