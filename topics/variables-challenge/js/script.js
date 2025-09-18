cdcd78f66fffe37364fced5b5db90b4731269aff
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-09-17 21:26:50 -0400

    make them all shake and mrfurious stops shaking at some point

--- topics/variables-challenge/js/script.js
+++ topics/variables-challenge/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * denver butter
+ * edmond and abhi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * mrfurious is super furious and bird going crazy
  */
 
 "use strict";
@@ -18,9 +17,9 @@ let skyShade = {
   }
 }
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * angry guy stops when bird shakes on top of him
 
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ 
 */
 
 
@@ -50,6 +49,19 @@ let mrFurious = {
   }
 };
 
+let drawBird = {
+   x: 200,
+  y: 200,
+  size: 50,
+  // Colour
+  fill: {
+    r: 53,
+    g: 49,
+    b: 49
+  }
+}
+
+
 /**
  * Create the canvas
  */
@@ -66,10 +78,11 @@ function draw() {
    skyShade.fill.g = skyShade.fill.g-1
   background(skyShade.fill.r,skyShade.fill.g,skyShade.fill.b);
 
+
+  
   mrFurious.fill.r = mrFurious.fill.r+1
   mrFurious.fill.g = mrFurious.fill.g-1
   mrFurious.fill.b = mrFurious.fill.b-1
-  
 
 
   // Draw Mr. Furious as a coloured circle
@@ -77,6 +90,60 @@ function draw() {
   noStroke();
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x/2, mrFurious.y/2, mrFurious.size);
-  pop();
+
+  fill(255,255,255)
+  ellipse(mrFurious.x/2.5, mrFurious.y/2, mrFurious.size/3)
+
+  fill(255,255,255)
+  ellipse(mrFurious.x/1.8, mrFurious.y/2, mrFurious.size/3)
+
+  fill(0,0,0)
+  ellipse(mrFurious.x/2.5, mrFurious.y/2, mrFurious.size/3.5)
+
+  fill(0,0,0)
+  ellipse(mrFurious.x/1.8, mrFurious.y/2, mrFurious.size/3.5)
+
+  fill(255,255,255)
+  rect(250,340,80,30)
+  
+  fill(0,0,0)
+  rect(250,340,80,20)
+
+drawBird.x =  drawBird.x+2
+drawBird.y =  random(50,300)
+
+
+
+
+if(drawBird.x < 700)
+{
+shake();
 }
+
+  pop();
+  annoying()
+}
+
+function annoying() {
+push();
+  noStroke();
+  fill(drawBird.fill.r, drawBird.fill.g, drawBird.fill.b);
+  ellipse(drawBird.x/2, drawBird.y/2, drawBird.size*1.5);
+
+    fill(53,49,49);
+  ellipse(drawBird.x/2.2, drawBird.y/2.1, drawBird.size*2);
+
+  fill(247,133,4);
+  ellipse(drawBird.x/2.3, drawBird.y/2.2, 60,20)
+  
+  pop();
+
+}
+
+function shake() {
+  mrFurious.x = random(580,600)
+mrFurious.y = random(500,600)
+
+}
+
 
