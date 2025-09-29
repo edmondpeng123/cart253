1f9235130f6cfe0b221d8ea77fb962d704d89178
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-09-29 00:05:26 -0400

    Put title and describe my project

--- topics/art-jam/js/script.js
+++ topics/art-jam/js/script.js
@@ -1,9 +1,18 @@
 /**
- * Title of Project
- * Author Name
+ * Mirror of Fragments
+ * Edmond Peng
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * 
+ * I want to make several petal shaped forms (fragment) all rotating around a circle; each petal shaped form will be an image 
+ * that is associated with my own perception of the self whether it be physical, psychological or anything visually or 
+ * emotionally represented. They will all rotate at different rhythms and overlap one another. When you move your mouse to a fragment, 
+ * it will make it grow bigger so that you can observe better but it will start to blur until it becomes indistinguishable. The petals
+ * is a visual representation of different facets of my individual although not directly referencing them; they flow in fluctuating
+ * rhythms to highlight its ever-changing quality and they all rotate around an invisible circle which represents the core/soul.
+ * The blurred effect of holding unto a petal is to replicate the ephemeral and, again, ever-changing quality of how I may
+ * associate things/body/ideas to my being, but may also lose them over time.
+ * 
+ * 
  */
 
 "use strict";
