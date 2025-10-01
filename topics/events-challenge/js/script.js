5bf06b43904c8708825dc036bd0c3230ed7b2124
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-10-01 19:34:02 -0400

    FINAL ADJUSTMENTS DESCRIPTIOS

--- topics/events-challenge/js/script.js
+++ topics/events-challenge/js/script.js
@@ -1,24 +1,99 @@
 /**
- * Title of Project
- * Author Name
+ * YOU LOSE TITLE
+ * Edmond Peng and AHMAD Alrazzaz
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * if you move anything you lose game
  */
 
 "use strict";
 
+//current score
+let score = 0;
+
+// Is the game over?
+let gameOver = false;
+
+let startingmouseX;
+let startingmouseY;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * canvas and x and y
 */
 function setup() {
+createCanvas(400, 400);
+
+startingmouseX = mouseX;
+startingmouseY = mouseY;
 
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ *update the score and display the UI
 */
 function draw() {
 
+
+    background("#87ceeb");
+  
+  // Only increase the score if the game is not over
+  if (!gameOver) {
+    // Score increases relatively slowly
+    score += 0.05;
+  }
+  displayUI();
+  checkForInput();
+}
+
+//show the game over if needed
+
+function displayUI() {
+  if (gameOver) {
+    push();
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text("You lose!", width/2, height/3);
+    pop();
+  }
+  displayScore();
+}
+
+/**
+ * Display the score
+ */
+function displayScore() {
+  push();
+  textSize(48);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+  text(floor(score), width/2, height/2);
+  pop();
+}
+
+
+function checkForInput() {
+if (
+    keyIsPressed ||
+    mouseIsPressed ||
+    mouseX !== startingmouseX ||
+    mouseY !== startingmouseY
+  ) {
+    lose();
+    }
+
+  document.addEventListener("visibilitychange", () => {
+    lose();
+  });
+
+  if (!document.hasFocus()) {
+    lose();
+  }
+
+  addEventListener("online", (event) => { lose(); })
+  addEventListener("offline", (event) => { lose(); })
+}
+
+function lose() {
+  gameOver = true;
 }
