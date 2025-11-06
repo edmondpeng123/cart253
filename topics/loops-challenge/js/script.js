fd17bedc4257bae17b619bba64b10f0e3d489a73
edmondpeng123 <166573341+edmondpeng123@users.noreply.github.com>
2025-11-05 20:07:52 -0500

    ADDED LINES AND GRADIENT
    r,jkrkgfj

--- topics/loops-challenge/js/script.js
+++ topics/loops-challenge/js/script.js
@@ -10,6 +10,10 @@
 /**
  * canva
 */
+
+
+
+
 function setup() {
     createCanvas(500, 500);
 }
@@ -20,40 +24,68 @@ function setup() {
  */
 function draw() {
 
-    background("black");
+     background("orange");
+
+let x = 50;
+let y = 25;
+
+
+for (let x = 1; x < 500; x += 1.5) {
+
+      stroke(0 + y)
+  line(x, 1, x, 500);
+
+  y+=0.5;
+
+}
+
+    while (x < 500) {
+stroke(0 + y)
+        line(x, 0, x, 500)
+
+
+        stroke(0 + y)
+        line(0, x, 500, x)
+
+
+        x+=50;
+        y +=25;
+    }
+
+
+
+    // stroke(0);
+    // line(0, 0, 0, height);
     
-    stroke(0);
-    line(0, 0, 0, height);
+    // stroke(25);
+    // line(50, 0, 50, height);
     
-    stroke(25);
-    line(50, 0, 50, height);
+    // stroke(50);
+    // line(100, 0, 100, height);
     
-    stroke(50);
-    line(100, 0, 100, height);
+    // stroke(75);
+    // line(150, 0, 150, height);
     
-    stroke(75);
-    line(150, 0, 150, height);
+    // stroke(100);
+    // line(200, 0, 200, height);
     
-    stroke(100);
-    line(200, 0, 200, height);
+    // stroke(125);
+    // line(250, 0, 250, height);
     
-    stroke(125);
-    line(250, 0, 250, height);
+    // stroke(150);
+    // line(300, 0, 300, height);
     
-    stroke(150);
-    line(300, 0, 300, height);
+    // stroke(175);
+    // line(350, 0, 350, height);
     
-    stroke(175);
-    line(350, 0, 350, height);
+    // stroke(200);
+    // line(400, 0, 400, height);
     
-    stroke(200);
-    line(400, 0, 400, height);
+    // stroke(225);
+    // line(450, 0, 450, height);
     
-    stroke(225);
-    line(450, 0, 450, height);
-    
-    stroke(250);
-    line(500, 0, 500, height);
+    // stroke(250);
+    // line(500, 0, 500, height);
 
 //     const CELL_SIZE = 25;
 // const NUM_COLS = 500/CELL_SIZE;
